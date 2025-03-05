eebb71f5c8660001cd7fecff0f0b549e5b184c59
April Gilmer <aprilgilmer1@gmail.com>
2025-03-04 20:07:27 -0600

    Completed LINQ Exerise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var gameNames = new List<string>()
+            {
+            "Fallout: New Vegas",
+            "Assettocorsa",
+            "Half Life",
+            "Legend of Zelda",
+            "Doom",
+            "Halo Reach",
+            "Mario"
+         };
+            var orderedGameNames = gameNames.OrderBy(gameName => gameName.Length).ToList();
+            orderedGameNames.ForEach(game => Console.WriteLine(game));
         }
     }
 }
